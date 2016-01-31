38c9dab22dc83b7453177d5920de696dcd62f31c
Irina Smirnova <smirnovapr@mail.ru>
2016-01-31 23:04:40 +0300

    Доработана интерактивная карта

--- js/map.js
+++ js/map.js
@@ -1,14 +1,15 @@
 function initialize() {
-    var mapCanvas = document.getElementById("map-canvas");
+    var mapCanvas = document.getElementById('map-canvas');
     var mapOptions = {
+        scrollwheel: false,
         center: new google.maps.LatLng(34.8937876,-111.7007464),
         zoom: 9,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
+    }
     var map = new google.maps.Map(mapCanvas, mapOptions);
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(34.8937876,-111.7007464),
         map: map,
     });
 }
-google.maps.event.addDomListener(window, "load", initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
